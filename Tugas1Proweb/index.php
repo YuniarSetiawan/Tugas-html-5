e82d5fbb52c08af513a698c07c0a075404acd063
YuniarSetiawan <37091876+YuniarSetiawan@users.noreply.github.com>
2018-03-06 21:49:23 +0700

    tugas html 5

---
+++ Tugas1Proweb/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>tugas proweb</title>
+        <link rel="icon"href="foto.jpg">
+    </head>
+    <body style="background-color:powderblue;">
+        
+        <h1 style="text-align: center; color:red"><i>TUGAS PROWEB 1</i></h1>
+        <hr style="color:red">
+        
+        <h3><big>PROFIL</big></h3>
+        <pre><b>
+        Nama : yuniar setiawan
+        kelas: informatika B
+        Nim  : 5150411076
+        <img src="foto.jpg" height="200"width="200"
+        </b>
+        </pre>
+        <div>
+        <p>
+            saya berasal dari gunungkidul, yogyakarta. tepatnya  di jati, botoayaaan, rongkop, gunungkidul. alamat jogja di trini,
+            trihanggo, gampiing, sleman, yogyakarta<br>
+        </div>
+            
+            hobi yang di tekuni:
+        <blockquote>
+            1.futsal <br>
+            2.memancing<br>
+            3.servis alat elektronik<br>
+        </blockquote>
+        </p>
+        <h4> Daftar pemrograman yang pernah di pelajari : </h4>
+        <ul>
+            <li>pemrograman java</li>
+            <li>pemrograman c#</li>
+            <li>pemrograman web</li>
+            <li>pemrograman c++</li>
+            <li>pemrograman client server</li>
+            
+        </ul>
+        <h2><u>riwayat pendidikan</u></h2>
+        <table style="border: 3px black solid">
+            <tr style="background-color: greenyellow">
+                <td style="border: 3px black solid"> NO</td>
+                <td style="border: 3px black solid"> NAMA SEKOLAH</td>
+                <td style="border: 3px black solid"> JURUSAN</td>
+                <td style="border: 3px black solid"> LULUS</td>
+            </tr>
+            <tr style="background-color: yellow">
+                <td style="border: 3px black solid"> 1</td>
+                <td style="border: 3px black solid"> SDN KENTHENG RONGKOP</td>
+                <td style="border: 3px black solid"> -</td>
+                <td style="border: 3px black solid"> 2009</td>
+            </tr>
+            <tr style="background-color: yellow">
+                <td style="border: 3px black solid"> 2</td>
+                <td style="border: 3px black solid"> MTS N RONGKOP</td>
+                <td style="border: 3px black solid"> -</td>
+                <td style="border: 3px black solid"> 2012</td>
+            </tr>
+            <tr style="background-color: yellow">
+                <td style="border: 3px black solid"> 3</td>
+                <td style="border: 3px black solid"> SMK MUHAMADIYAN 1 PLAYEN </td>
+                <td style="border: 3px black solid"> TEKNIK AUDIO VIDEO</td>
+                <td style="border: 3px black solid"> 2015</td>
+            </tr>
+            
+        </table>
+        <hr>
+        <h3>JADWAL KULIAH</h3>
+        <table style="border: 3px black solid">
+            <tr style="background-color: red">
+                <td style="border: 3px black solid"> HARI</td>
+                <td style="border: 3px black solid"> MAKUL</td>
+                <td style="border: 3px black solid"> RUANG</td>
+                <td style="border: 3px black solid"> JAM</td>
+            </tr>
+            <tr style="background-color: greenyellow">
+                <td style="border: 3px black solid"> SENIN</td>
+                <td style="border: 3px black solid"> ADINISTRASI WEB SERVER</td>
+                <td style="border: 3px black solid"> D.01</td>
+                <td style="border: 3px black solid"> 12.50-15.20</td>
+            </tr>
+            <tr style="background-color: yellow">
+                <td style="border: 3px black solid"> SELASA</td>
+                <td style="border: 3px black solid"> PEMROGRAMAN WEB PRAKTEK</td>
+                <td style="border: 3px black solid"> LK.10</td>
+                <td style="border: 3px black solid"> 14.40-18.00</td>
+            </tr>
+            <tr style="background-color: greenyellow">
+                <td style="border: 3px black solid"> RABU</td>
+                <td style="border: 3px black solid"> PEMROGRAMAN WEB</td>
+                <td style="border: 3px black solid"> F.01</td>
+                <td style="border: 3px black solid"> 14.40-16.20</td>
+            </tr>
+            <tr  style="background-color: yellow">
+                <td style="border: 3px black solid"rowspan="2"> KAMIS</td>
+                <td style="border: 3px black solid"> REKAYASA WEB</td>
+                <td style="border: 3px black solid"> D.03</td>
+                <td style="border: 3px black solid"> 09.40-12.10</td>
+            </tr>
+            <tr style="background-color: yellow">
+                <td style="border: 3px black solid"> PEM. CLIENT & SERVER</td>
+                <td style="border: 3px black solid"> D.33</td>
+                <td style="border: 3px black solid"> 14.40-16.20</td>
+            </tr>
+            <tr style="background-color: greenyellow">
+                <td style="border: 3px black solid"rowspan="2"> JUMAT</td>
+                <td style="border: 3px black solid"> ETIKA PROFESI</td>
+                <td style="border: 3px black solid"> D.34</td>
+                <td style="border: 3px black solid"> 09.40-11.20</td>
+            </tr>
+            <tr style="background-color: greenyellow">
+                <td style="border: 3px black solid"> INFORMATIKA TELEKOMUNIKASI</td>
+                <td style="border: 3px black solid"> D.03</td>
+                <td style="border: 3px black solid"> 15.30-18.00</td>
+            </tr>
+            <tr style="background-color:yellow">
+                <td style="border: 3px black solid"> SABTU</td>
+                <td style="border: 3px black solid"> PEM.CLIENT & SERRVER PRAKTEK</td>
+                <td style="border: 3px black solid"> LK.09</td>
+                <td style="border: 3px black solid"> 13.40-17.00</td>
+            </tr>
+        </table>
+        <hr>
+        <h1><marquee style="font-family:verdana">2018 2018 2018 2018</marquee></h1>
+        <aside>
+            <h3>yuniar setiawan</h3>
+        </aside>
+        <footer>
+            <p>copyright 2018 yuniar setiawan</p>
+        </footer>
+    </body>
+</html>
